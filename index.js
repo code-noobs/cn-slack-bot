7ab83a4ce105324c6b05019916e013df58f326fd
root <root@debian>
2018-07-07 17:34:02 -0600

    working connection

---
+++ index.js
@@ -0,0 +1,19 @@
+const SlackBot = require('slackbots');
+const axios = require('axios');
+
+const bot = new SlackBot({
+	token: 'xoxb-299068034276-394502308610-JdAWPgDv548gGU5ZsPAsnWzM',
+	name: 'Code Noobs Bot'
+});
+
+// Start Handler
+bot.on('start', () => {
+	const params = {
+	icon_emoji: ':smiley:'
+	};
+
+	bot.postMessageToChannel('general', 'ITSWORKING.JPG', params);
+});
+
+// Error Handler
+bot.on('error', err => console.log(err));
