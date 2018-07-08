850d67802e3ee7ac3862241bf0d0a17da90d661d
root <root@debian>
2018-07-07 18:56:40 -0600

    Added Last.FM API, but only works with URL

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const bot = new SlackBot({
 // Start Handler
 bot.on('start', () => {
 	const params = {
-	icon_emoji: ':smiley:'
+	icon_emoji: ':wave:'
 	};
 
 	bot.postMessageToChannel('bot-testing', "Don't panic", params);
@@ -17,3 +17,32 @@ bot.on('start', () => {
 
 // Error Handler
 bot.on('error', err => console.log(err));
+
+// Mesage Handler
+bot.on('message', (data) => {
+	if(data.type !== 'message'){
+	return;
+	}
+
+	handleMessage(data.text);
+});
+
+// Respond to Data
+function handleMessage(message) {
+	if(message.includes(' lastfm')) {	
+	lastfmArtist();
+	}
+}
+
+// Pull Last.FM artist information
+function lastfmArtist() {
+  axios.get('http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=prince&api_key=3cd4582d91ac24b7a041e998287b26d8&format=json').then(res => {
+    console.log(res.data.artist['name']);    
+    const artist = res.data.artist.name;
+    const params = {
+      icon_emoji: ':musical_score:'
+    };
+
+    bot.postMessageToChannel('bot-testing', artist, params);
+  });
+}
