af3132024cf2debacc1729ac32644ff2757e8255
Kyle OBrien <obrienk@webbhost.net>
2018-07-08 00:53:34 -0500

    added whois functionality, added https and querystring modules

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const SlackBot = require('slackbots');
 const axios = require('axios');
+const https = require('https');
+const querystring = require('querystring');
 
 const bot = new SlackBot({
 	token: 'xoxb-299068034276-394502308610-JdAWPgDv548gGU5ZsPAsnWzM',
 	name: 'Code Noobs Bot'
 });
 
-const dnsapi = 'https://dns-api.org/';
+const dnsApi = 'https://dns-api.org/a/webbhost.net';
 
 // Start Handler
 bot.on('start', () => {
@@ -23,24 +25,35 @@ bot.on('error', err => console.log(err));
 // Mesage Handler
 bot.on('message', (data) => {
 	if(data.type !== 'message'){
+	
+		
+		
 	return;
 	}
-
+	console.log(data);
 	handleMessage(data.text);
 });
 
+
+
 // Respond to Data
 function handleMessage(message) {
-	if(message.includes(' dns')) {	
-	dnsLookup();
+	if(message.includes(' dns')) {
+		
+		console.log(message);
+		dnsLookup();
+	} else if(message.includes(' whois')) {
+		whoisLookup();
 	}
 }
 
 // Pull dns-api.org information
 function dnsLookup() {
-  axios.get('dnsapi').then(res => {
+  axios.get(dnsApi).then(res => {
        
 	const dns = res.data;
+	
+
 	console.log(dns); 
 
     const params = {
@@ -49,4 +62,48 @@ function dnsLookup() {
 
     bot.postMessageToChannel('bot-testing', dns, params);
   });
+}
+
+// integrate whois API
+function whoisLookup(){
+var url = "https://www.whoisxmlapi.com/"
+    +"whoisserver/WhoisService?";
+var parameters = {
+		domainName: 'google.com',
+		apiKey: 'at_eFYe3jSZXRIcAVywg2QKj9sbQjqzh',
+		outputFormat: 'json'
+};
+url = url + querystring.stringify(parameters);
+
+https.get(url, function (res) {
+    const statusCode = res.statusCode;
+
+    if (statusCode !== 200) {
+        console.log('Request failed: '
+            + statusCode
+        );
+    }
+
+    var rawData = '';
+
+    res.on('data', function(chunk) {
+        rawData += chunk;
+    });
+    res.on('end', function () {
+        try {
+            var parsedData = JSON.parse(rawData);
+            if (parsedData.WhoisRecord) {
+                bot.postMessageToChannel('bot-testing','Domain name: '+ parsedData.WhoisRecord.domainName);
+				bot.postMessageToChannel('bot-testing','Contact email: '+ parsedData.WhoisRecord.contactEmail);
+            } else {
+                console.log(parsedData);
+            }
+        } catch (e) {
+            console.log(e.message);
+        }
+    })
+}).on('error', function(e) {
+    console.log("Error: " + e.message);
+});
+
 }
