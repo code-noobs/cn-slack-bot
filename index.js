0c100f7399b02dfe2d2bf7b66aa0a68c1e978bce
Kyle OBrien <obrienk@webbhost.net>
2018-08-10 17:00:53 -0500

    parsed URL data and correctly processing through APIs. End result works, still needs more cleanup and message posting could be improved

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const SlackBot = require('slackbots');
 const axios = require('axios');
 const https = require('https');
 const querystring = require('querystring');
-
+const env = require('dotenv');
+env.config({path: 
+        '.env'});
 const bot = new SlackBot({
-	token: 'YOUR_SLACK_TOKEN',
+	token: process.env.SLACK_API_TOKEN,
 	name: 'Code Noobs Bot'
 });
 
@@ -35,40 +37,105 @@ bot.on('message', (data) => {
 
 // Respond to Data
 function handleMessage(message) {
-    if(message.includes('\<\@')){
+    if(message.includes('dns')) {
+        // Take the data I want and re-organize for the API to use
+        // This removes the user ID text <@XXXXXXXXX>
+        var buf1 = Buffer.allocUnsafe(26);
+        buf1 = message;
+        buf2 = buf1.slice(13, buf1.length);
+        message = buf2.toString('ascii', 0, buf2.length);
+
+        /* There may be a better way with the Slack API, but this will work
+        returns URL as a string without extra formatting. Pre-formatted text appears like:
+        <http://webbhost.net|webbhost.net> */
+        var n = message.indexOf('|');
+        var o = message.indexOf('>');
+        console.log(n);
+        console.log(o);
+        console.log(o-1);
+        var n = n+1;
+        var o = o-2;
+        var s1 = message.substr(n, o);
+        console.log(s1+"s1");
+        var p = s1.indexOf('>');
+        var s2 = s1.substr(0, p);
+        console.log(s2+"s2");
+        message = s2;
+        dnsLookup(message);
+        
+	} else if(message.includes(' whois')) {
         // Take the data I want and re-organize for the API to use
         // This should probably be it's own function
         var buf1 = Buffer.allocUnsafe(26);
         buf1 = message;
         buf2 = buf1.slice(13, buf1.length);
         message = buf2.toString('ascii', 0, buf2.length);
-        console.log(message);
-        function dnsPath(message) {
-            var data = message.toString();
-            console.log(data);
-        }       
+        console.log(message); // first message
+
         // Select part of message to remove
         // returns null
         //returns input.. and stuff posted here: https://webbhost.slack.com/archives/CBLERPJCA/p1531193642000053
         
-        var regex = new RegExp(/^([<a-zA-Z:\/\/a-zA-Z\.a-zA-Z\|>])\w+/);
-        var arrMatches = message.match(regex);
-        console.log(arrMatches);
-        message = arrMatches;
-        return message;
-        dnsLookup(message);
-        
-    } else if(message.includes(' dns')) {
-        dnsLookup()
-        
-	} else if(message.includes(' whois')) {
+
+        /* Let's try not using regex shall we? */
+        var n = message.indexOf('|');
+        var o = message.indexOf('>');
+        console.log(n);
+        console.log(o);
+        console.log(o-1);
+        var n = n+1;
+        var o = o-2;
+        var s1 = message.substr(n, o);
+        console.log(s1+"s1");
+        var p = s1.indexOf('>');
+        var s2 = s1.substr(0, p);
+        console.log(s2+"s2");
+        message = s2;
 		whoisLookup(message);
 	}
 }
 
+
+
 // Pull dns-api.org information
-function dnsLookup() {
-    axios.get(dnsApi + '\/A' + '\/webbhost.net').then(res => {
+function dnsLookup(message) {
+    axios.get(dnsApi + '\/NS' + '\/'+ message).then(res => {
+    
+        const dns = res.data;
+    
+        console.log(dns); 
+    
+        const params = {
+          icon_emoji: ''
+        };
+    
+        bot.postMessageToChannel('bot-testing', dns, params);
+      });
+    axios.get(dnsApi + '\/A' + '\/'+ message).then(res => {
+    
+	const dns = res.data;
+
+	console.log(dns); 
+
+    const params = {
+      icon_emoji: ''
+    };
+    axios.get(dnsApi + '\/CNAME' + '\/'+ message).then(res => {
+    
+        const dns = res.data;
+    
+        console.log(dns); 
+    
+        const params = {
+          icon_emoji: ''
+        };
+    
+        bot.postMessageToChannel('bot-testing', dns, params);
+      });
+
+    bot.postMessageToChannel('bot-testing', dns, params);
+  });
+  axios.get(dnsApi + '\/MX' + '\/'+ message).then(res => {
     
 	const dns = res.data;
 
@@ -80,15 +147,28 @@ function dnsLookup() {
 
     bot.postMessageToChannel('bot-testing', dns, params);
   });
+  axios.get(dnsApi + '\/TXT' + '\/'+ message).then(res => {
+    
+	const dns = res.data;
+
+	console.log(dns); 
+
+    const params = {
+      icon_emoji: ''
+    };
+
+    bot.postMessageToChannel('bot-testing', dns, params);
+  });
+
 }
 
 // integrate whois API
-function whoisLookup(){
+function whoisLookup(message){
 var url = "https://www.whoisxmlapi.com/"
     +"whoisserver/WhoisService?";
 var parameters = {
-		domainName: 'webbhost.net',
-		apiKey: 'YOUR_WHOIS_API_KEY',
+		domainName: message,
+		apiKey: process.env.WHOIS_API_KEY,
 		outputFormat: 'json'
 };
 url = url + querystring.stringify(parameters);
@@ -112,7 +192,13 @@ https.get(url, function (res) {
             var parsedData = JSON.parse(rawData);
             if (parsedData.WhoisRecord) {
                 bot.postMessageToChannel('bot-testing','Domain name: '+ parsedData.WhoisRecord.domainName);
-				bot.postMessageToChannel('bot-testing','Contact email: '+ parsedData.WhoisRecord.contactEmail);
+                bot.postMessageToChannel('bot-testing','Contact email: '+ parsedData.WhoisRecord.contactEmail);
+                bot.postMessageToChannel('bot-testing','Created date: '+ parsedData.WhoisRecord.createdDate);
+                bot.postMessageToChannel('bot-testing','Updated date: '+ parsedData.WhoisRecord.updatedDate);
+                bot.postMessageToChannel('bot-testing','Expired date: '+ parsedData.WhoisRecord.expiresDate);
+                bot.postMessageToChannel('bot-testing', parsedData.WhoisRecord.registrant.rawText);
+                bot.postMessageToChannel('bot-testing', parsedData.WhoisRecord.administrativeContact.rawText);
+                
             } else {
                 console.log(parsedData);
             }
